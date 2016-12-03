457207fb11eb0afedfea8329fa74ebee8428029a
magda <magdamalecka@icloud.com>
2016-12-03 15:25:51 +0100

    Deserialization Class - it deserialize employee objects

--- src/main/java/DeserializationClass.java
+++ src/main/java/DeserializationClass.java
@@ -1,7 +1,29 @@
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
 
 public class DeserializationClass {
 
     public static void main(String[] args) {
+
+        Employee emp = null;
+
+        try {
+            FileInputStream inputFile = new FileInputStream("./emplyee.txt");
+            ObjectInputStream input = new ObjectInputStream(inputFile);
+            emp = (Employee) input.readObject();
+
+            inputFile.close();
+            input.close();
+
+        } catch (java.io.IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            System.out.println("Class not found");
+            e.printStackTrace();
+        }
+
+        System.out.println("Deserialized object: ");
+        System.out.println("Employee: " + emp.getName() + " " + emp.getSurname() + " ID" + emp.getId());
 
     }
 }
--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -10,4 +10,16 @@ public class Employee implements Serializable {
         this.name = name;
         this.surname = surname;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public int getId() {
+        return ID;
+    }
 }
